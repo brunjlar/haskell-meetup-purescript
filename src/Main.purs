932f164073b16ac232ee0ac225b4897508eaf460
Lars Brünjes <brunjlar@gmail.com>
2015-08-04 00:37:23 +0200

    row polymorphism (birthday)

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,20 @@
 module Main where
 
 import Control.Monad.Eff.Console
+import Prelude
+
+type HasAge r = { age :: Int | r }
+
+birthday :: forall r. HasAge r -> HasAge r
+birthday p = p { age = p.age + 1 }
+
+type Person = { name :: String, age :: Int}
+
+showPerson :: Person -> String
+showPerson p = p.name ++ "(" ++ show p.age ++ ")"
+
+lars :: Person
+lars = { name : "Lars", age : 43 }
 
 main = do
-  log "Hello sailor!"
+    log $ showPerson $ birthday lars
